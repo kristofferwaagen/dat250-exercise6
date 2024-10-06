1a2469321ac4a3b563ba42dadfe154cf638f6990
kristofferwaagen <kristofferwagen@gmail.com>
2024-10-06 19:53:42 +0200

    Finished Work Queues

--- src/main/java/org/example/Recv.java
+++ src/main/java/org/example/Recv.java
@@ -3,20 +3,34 @@ package org.example;
 import com.rabbitmq.client.*;
 
 public class Recv {
-    private final static String QUEUE_NAME = "hello";
+    private final static String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages...");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                System.out.println(" [x] Done");
+            }
         };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        channel.basicConsume(TASK_QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
     }
 }
--- src/main/java/org/example/Send.java
+++ src/main/java/org/example/Send.java
@@ -5,17 +5,21 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Send {
-    private final static String QUEUE_NAME = "hello";
+    private final static String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println(" [x] Sent '" + message + "'");
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String[] messages = {"Task 1.....", "Task 2.....", "Task 3....."};
+
+            for (String message : messages) {
+                channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes("UTF-8"));
+                System.out.println(" [x] Sent '" + message + "'");
+            }
         }
     }
 }
