accf25e2c54d34d82dbeb51f1fa6f03ad751e59c
kristofferwaagen <kristofferwagen@gmail.com>
2024-10-06 20:19:33 +0200

    Completed Topics

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Start the RabbitMQ Receiver
-            Recv.main(new String[]{});
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose Experiment to run: (2) Work Queues, (4) Publish/Subscribe");
+        int choice = scanner.nextInt();
 
-            // Start the RabbitMQ Sender
-            Send.main(new String[]{});
+        try {
+            if (choice == 2) {
+                // Start Experiment 2/3: Work Queues
+                Recv.main(new String[]{});
+                Send.main(new String[]{});
+            } else if (choice == 4) {
+                // Start Experiment 4: Publish/Subscribe
+                EmitLog.main(new String[]{});
+                ReceiveLogs.main(new String[]{});
+            } else {
+                System.out.println("Invalid choice. Please choose 2 or 4.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
